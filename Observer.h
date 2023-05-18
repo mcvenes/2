805dcc77a9286542ea71865ba16714033b808a43
mcvenes <102212737+mcvenes@users.noreply.github.com>
2023-05-19 02:25:29 +0700

    Update Observer.h

--- Observer.h
+++ Observer.h
@@ -1,10 +1,14 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
-class Observer {
+#include <QObject>
+
+class Observer : public QObject {
+    Q_OBJECT
+
 public:
-    virtual void UpdateExist(bool exist) = 0;
-    virtual void UpdateSize(int size) = 0;
+    virtual void UpdateExist(bool exist) = 0;  // Виртуальный метод для обновления состояния существования файла
+    virtual void UpdateSize(int size) = 0;    // Виртуальный метод для обновления размера файла
 };
 
 #endif // OBSERVER_H
