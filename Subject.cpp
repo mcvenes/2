6b39c2174ab32bbde8a7bf0fae2e1a848c1b19da
mcvenes <102212737+mcvenes@users.noreply.github.com>
2023-05-19 02:26:00 +0700

    Update Subject.cpp

--- Subject.cpp
+++ Subject.cpp
@@ -1,17 +1,18 @@
 #include "Subject.h"
 
 void Subject::Attach(MyFile* file) {
-    files.push_back(file);
+    files.push_back(file);  // Добавляем указатель на файл в вектор наблюдателей
 }
 
 void Subject::Detach(MyFile* file) {
-    files.erase(std::remove(files.begin(), files.end(), file), files.end());
+    files.erase(std::remove(files.begin(), files.end(), file), files.end());  // Удаляем указатель на файл из вектора наблюдателей
 }
 
 void Subject::NotifyExist(bool exist) {
     for (auto iter = files.begin(); iter != files.end(); ++iter) {
         if (*iter != nullptr) {
-            (*iter)->UpdateExist(exist);
+            (*iter)->UpdateExist(exist);  // Вызываем метод обновления состояния существования файла у каждого наблюдателя
+            emit onExist(*iter); // Генерируем сигнал onExist для оповещения об изменении состояния существования файла
         }
     }
 }
@@ -19,7 +20,7 @@ void Subject::NotifyExist(bool exist) {
 void Subject::NotifySize(int size) {
     for (auto iter = files.begin(); iter != files.end(); ++iter) {
         if (*iter != nullptr) {
-            (*iter)->UpdateSize(size);
+            (*iter)->UpdateSize(size);  // Вызываем метод обновления размера файла у каждого наблюдателя
         }
     }
 }
