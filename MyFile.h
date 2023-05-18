2f8c19e95a65a0f27f1463533655333a4e5d3b9c
mcvenes <102212737+mcvenes@users.noreply.github.com>
2023-05-19 02:24:58 +0700

    Update MyFile.h

--- MyFile.h
+++ MyFile.h
@@ -1,19 +1,22 @@
 #ifndef MYFILE_H
 #define MYFILE_H
 
-#include "Observer.h"
 #include <string>
+#include "Observer.h"
 
 class MyFile : public Observer {
 private:
-    std::string path;
-    int size;
-    bool exist;
+    std::string path;    // Путь к файлу
+    int size;            // Размер файла
+    bool exist;          // Флаг существования файла
 
 public:
-    MyFile(std::string path_);
-    void UpdateExist(bool exist_) override;
-    void UpdateSize(int size_) override;
+    MyFile(std::string path_);      // Конструктор класса MyFile
+    void UpdateExist(bool exist_) override;  // Метод для обновления состояния существования файла
+    void UpdateSize(int size_) override;     // Метод для обновления размера файла
+    std::string getPath() const;             // Метод для получения пути к файлу
+    int getSize() const;                     // Метод для получения размера файла
+    bool getExist() const;                   // Метод для получения состояния существования файла
 };
 
 #endif // MYFILE_H
