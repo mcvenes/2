665c3bffa10aea82cabcdb89bc983f533c64c145
mcvenes <102212737+mcvenes@users.noreply.github.com>
2023-05-19 02:25:46 +0700

    Update Subject.h

--- Subject.h
+++ Subject.h
@@ -1,18 +1,25 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 
+#include <QObject>
 #include <vector>
 #include <algorithm>
 #include "MyFile.h"
 
-class Subject {
-    std::vector<MyFile*> files;
+class Subject : public QObject {
+    Q_OBJECT
+
+private:
+    std::vector<MyFile*> files;  // Вектор указателей на объекты MyFile
+
+signals:
+    void onExist(MyFile* file);  // Сигнал для оповещения о состоянии существования файла
 
 public:
-    void Attach(MyFile* file);
-    void Detach(MyFile* file);
-    void NotifyExist(bool exist);
-    void NotifySize(int size);
+    void Attach(MyFile* file);  // Метод для присоединения наблюдателя (файла)
+    void Detach(MyFile* file);  // Метод для отсоединения наблюдателя (файла)
+    void NotifyExist(bool exist);  // Метод для оповещения о состоянии существования файла
+    void NotifySize(int size);  // Метод для оповещения о размере файла
 };
 
 #endif // SUBJECT_H
